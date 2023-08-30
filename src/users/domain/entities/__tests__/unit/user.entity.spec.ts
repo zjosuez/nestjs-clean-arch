5268c0e8e7b175fa14b5e15c4f0c3b13d958c838
Josué Teixeira <zjosuez@outlook.com>
2023-08-30 17:02:23 -0300

    Criação da user entity com os testes unitários

---
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,25 @@
+import { UserProps } from './../../user.entity'
+import { faker } from '@faker-js/faker'
+import { UserEntity } from '../../user.entity'
+
+describe('Teste unitário UserEntity', () => {
+  let props: UserProps
+  let sut: UserEntity
+
+  beforeEach(() => {
+    props = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    }
+
+    sut = new UserEntity(props)
+  })
+
+  it('Metodo construtor', () => {
+    expect(sut.props.name).toEqual(props.name)
+    expect(sut.props.email).toEqual(props.email)
+    expect(sut.props.password).toEqual(props.password)
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
+})
