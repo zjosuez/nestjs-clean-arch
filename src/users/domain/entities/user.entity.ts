e1d525c906f4d929030e871cdad389cbb812ffd7
Josué Teixeira <zjosuez@outlook.com>
2023-09-04 21:13:01 -0300

    Atualiza os métodos

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -16,8 +16,24 @@ export class UserEntity extends Entity<UserProps> {
     this.props.createdAt = this.props.createdAt ?? new Date()
   }
 
+  update(value: string): void {
+    this.name = value
+  }
+
+  updatePassword(value: string): void {
+    this.password = value
+  }
+
   get name() {
     return this.props.name
+  }
+
+  private set name(value: string) {
+    this.props.name = value
+  }
+
+  private set password(value: string) {
+    this.props.password = value
   }
 
   get email() {
